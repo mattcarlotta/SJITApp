bb76bca957d341c476e143730be5fce796723db6
Matt <carlotta.matt@gmail.com>
2019-06-04 16:55:04 -0700

    Add redis and update README

--- middlewares/index.js
+++ middlewares/index.js
@@ -3,41 +3,45 @@ import cors from "cors";
 import morgan from "morgan";
 import passport from "passport";
 import session from "express-session";
-import connectMongo from "connect-mongo";
+import connectRedis from "connect-redis";
 import mongodbConnection from "database";
 import config from "env";
 // import userModel from "models/user";
 
-const { DATABASE, CLIENT, NODE_ENV } = process.env;
+const { CLIENT, NODE_ENV } = process.env;
 const inTesting = NODE_ENV === "test";
 
-const MongoStore = connectMongo(session);
+const RedisStore = connectRedis(session);
 //= ===========================================================//
 /* APP MIDDLEWARE */
 //= ===========================================================//
-export default app => {
+export default (app) => {
   mongodbConnection();
   // userModel();
   app.use(
     session({
       secret: config[NODE_ENV].cookieKey,
-      maxAge: 30 * 24 * 60 * 60 * 1000, // expire after 30 days, 30days/24hr/60m/60s/1000ms
+      name: "SJITApp",
       saveUninitialized: false, // don't create session until something stored
-      resave: false, //don't save session if unmodified
-      store: new MongoStore({
-        url: DATABASE,
-        name: "SJIceTeamMember",
-        touchAfter: 24 * 3600, // time period in between refreshing a session
-        autoRemoveInterval: 10, // interval between checking to remove old sessions
-        autoRemove: "native" // allow Mongo to automatically remove old sessions
-      })
-    })
+      resave: false, // don't save session if unmodified
+      cookie: {
+        path: "/",
+        httpOnly: true,
+        secure: process.env.protocol === "https",
+        maxAge: 30 * 24 * 60 * 60 * 1000, // expire after 30 days, 30days/24hr/60m/60s/1000ms
+      },
+      store: new RedisStore({
+        db: 0,
+        host: "127.0.0.1",
+        port: 6379,
+      }),
+    }),
   );
   app.use(
     cors({
       credentials: true,
-      origin: CLIENT
-    })
+      origin: CLIENT,
+    }),
   ); // allows receiving of cookies/tokens from front-end
   if (!inTesting) app.use(morgan("tiny")); // logging framework
   app.use(bodyParser.json()); // parses header requests (req.body)
