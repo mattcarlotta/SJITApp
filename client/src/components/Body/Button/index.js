ccfee8e6f5455daa62a16e3a9e2b05f125a4b1f9
Matt <carlotta.matt@gmail.com>
2019-06-10 20:50:37 -0700

    Remove prettier command on commits

--- client/src/components/Body/Button/index.js
+++ client/src/components/Body/Button/index.js
@@ -1,13 +1,13 @@
-import styled from 'styled-components';
-import StyledButton from './Button';
+import styled from "styled-components";
+import StyledButton from "./Button";
 
 const Button = styled(StyledButton)`
 	cursor: pointer;
-	color: ${props => (!props.primary && !props.danger ? '#025f6d' : '#ffffff')};
+	color: ${props => (!props.primary && !props.danger ? "#025f6d" : "#ffffff")};
 	background-color: ${props => {
-		if (props.primary) return '#025f6d';
-		if (props.danger) return '#f56342';
-		return 'transparent';
+		if (props.primary) return "#025f6d";
+		if (props.danger) return "#f56342";
+		return "transparent";
 	}};
 	text-decoration: none;
 	margin-right: 20px;
@@ -16,9 +16,9 @@ const Button = styled(StyledButton)`
 	border-radius: 4px;
 	border: 2px solid
 		${props => {
-			if (props.primary) return '#025f6d';
-			if (props.danger) return '#f56342';
-			return '#025f6d';
+			if (props.primary) return "#025f6d";
+			if (props.danger) return "#f56342";
+			return "#025f6d";
 		}};
 	width: 100%;
 	padding: 16px 18px 17px;
@@ -27,13 +27,13 @@ const Button = styled(StyledButton)`
 
 	&:hover {
 		color: ${props =>
-			!props.primary && !props.danger ? '#04515d' : '#ffffff'};
+			!props.primary && !props.danger ? "#04515d" : "#ffffff"};
 		background-color: ${props => {
-			if (props.primary) return '#04515d';
-			if (props.danger) return '#d24b2e';
-			return 'transparent';
+			if (props.primary) return "#04515d";
+			if (props.danger) return "#d24b2e";
+			return "transparent";
 		}};
-		border: 2px solid ${props => (props.danger ? '#d24b2e' : '#04515d')};
+		border: 2px solid ${props => (props.danger ? "#d24b2e" : "#04515d")};
 	}
 
 	&:focus {
