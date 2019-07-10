e10565a1e2f502a70576906ba170e2b298ce0dae
Matt <carlotta.matt@gmail.com>
2019-07-10 16:54:17 -0700

    Downgrade ESLint and add console messages

--- app.js
+++ app.js
@@ -1,2 +1,2 @@
 require("@babel/register");
-require("./utils/startServer");
+require("./server/server");
--- database/index.js
+++ database/index.js
@@ -1,6 +1,8 @@
 /* eslint-disable */
 import bluebird from "bluebird";
 import mongoose from "mongoose";
+import chalk from "chalk";
+const { log } = console;
 
 const { DATABASE, NODE_ENV } = process.env;
 const inTesting = NODE_ENV === "testing";
@@ -21,23 +23,42 @@ mongoose.Promise = bluebird; // bluebird for mongoose promises
 if (!inTesting) {
   mongoose.connection.on(
     "connected",
-    () => console.log(`\u001b[32mConnected to ${DATABASE}\u001b[0m\n`), // log mongodb connection established
+    () =>
+      log(
+        `\n${chalk.rgb(7, 54, 66).bgRgb(38, 139, 210)(" I ")} ${chalk.blue(
+          `Connected to ${DATABASE}`,
+        )}\n`,
+      ), // log mongodb connection established
   );
 
   mongoose.connection.on(
     "disconnected",
-    () => console.log(`\n\u001b[36mDisconnected from ${DATABASE}\u001b[0m`), // log mongodb connection disconnected
+    () =>
+      log(
+        `\n${chalk.rgb(7, 54, 66).bgRgb(38, 139, 210)(" I ")} ${chalk.rgb(
+          34,
+          155,
+          127,
+        )(`Disconnected from ${DATABASE}`)}`,
+      ), // log mongodb connection disconnected
   );
 
   mongoose.connection.on(
     "error",
-    () => console.log(`\u001b[31mConnection error to ${DATABASE}\u001b[0m\n`), // log mongodb connection error
+    () =>
+      log(
+        `\n${chalk.rgb(7, 54, 66).bgRgb(38, 139, 210)(" I ")} ${chalk.red(
+          `Connection error to ${DATABASE}`,
+        )}`,
+      ), // log mongodb connection error
   );
 
   process.on("SIGINT", () => {
     mongoose.connection.close(() => {
-      console.log(
-        `\u001b[35mConnection was manually terminated from ${DATABASE}\u001b[0m`, // log mongodb manual disconnection
+      log(
+        `${chalk.rgb(7, 54, 66).bgRgb(38, 139, 210)(" I ")} ${chalk.magenta(
+          `Connection was manully terminated from ${DATABASE}`,
+        )}`,
       );
       process.exit(0);
     });
