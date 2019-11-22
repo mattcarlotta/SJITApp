ddab6a2c5885224ad648042d9b8cfc40e7e759e8
Matt <carlotta.matt@gmail.com>
2019-11-22 15:54:58 -0800

    Add missing icons

--- client/src/utils/icons/index.js
+++ client/src/utils/icons/index.js
@@ -1,3 +1,4 @@
 export { default as CalendarOutline } from "@ant-design/icons/lib/outline/CalendarOutline";
+export { default as DownOutline } from "@ant-design/icons/lib/outline/DownOutline";
 export { default as LeftOutline } from "@ant-design/icons/lib/outline/LeftOutline";
 export { default as RightOutline } from "@ant-design/icons/lib/outline/RightOutline";
